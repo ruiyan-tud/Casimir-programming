6594855d9a2927c33ed4fd18fd35a642aa64fe1d
Rui <yanruice@gmail.com>
2019-11-06 16:06:51 +0100

    second try

--- mypro.py
+++ mypro.py
@@ -1,2 +1,19 @@
 #good luck!
-
+text = open('R_80_25_1,01.inp', 'r+')
+x = " 0.0033 "
+y = "-0.0027"
+z = " 0.0003 "
+xi = " 0.0039 "
+yi = " -0.0021 "
+zi = " 0.0009 "
+for line in text:    
+    if x,y,z in line:
+        text.write(line.replace(x, xi))
+        print line
+        text.write(line.replace(y, yi))
+        print line
+        text.write(line.replace(z, zi))
+        print line
+    else:
+        print line
+text.close()
